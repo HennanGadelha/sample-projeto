3f7192cb609c8d3636e48855155cd1ac4fd2643d
hennangadelha <hennanhennan@gmail.com>
2021-09-20 16:20:07 -0300

    implementando delecao de instituicao

---
+++ src/main/java/com/sampleprojeto/instituicao/controller/DeletandoInstituicaoController.java
@@ -0,0 +1,25 @@
+package com.sampleprojeto.instituicao.controller;
+
+import com.sampleprojeto.instituicao.repository.InstituicaoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(("/api/v1/instituicoes"))
+public class DeletandoInstituicaoController {
+
+    @Autowired
+    private InstituicaoRepository repository;
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deletandoInstituicao(@PathVariable String id){
+
+        repository.deleteById(id);
+        return  ResponseEntity.ok().build();
+    }
+
+}
--- src/main/java/com/sampleprojeto/instituicao/repository/InstituicaoRepository.java
+++ src/main/java/com/sampleprojeto/instituicao/repository/InstituicaoRepository.java
@@ -3,7 +3,10 @@ package com.sampleprojeto.instituicao.repository;
 import com.sampleprojeto.instituicao.model.Instituicao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface InstituicaoRepository extends JpaRepository<Instituicao, String> {
+
+
 }
