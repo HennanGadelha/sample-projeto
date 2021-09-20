7c59d31026464aade5b2a7b8b8eebad7e9233977
hennangadelha <hennanhennan@gmail.com>
2021-09-20 00:57:26 -0300

    criando projeto

---
+++ src/test/java/com/sampleprojeto/SampleprojetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sampleprojeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleprojetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
